4149c4c960b59ad087790bcee1f3437ff6b7f73b
apsamuel <aaron.psamuel@gmail.com>
2022-04-10 13:41:27 -0400

    feat: use react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react'
-import { ThemeProvider, createTheme } from '@mui/material'
-import { darkTheme, lightTheme } from './themes/basic'
+// import ReactDom from 'react-dom'
+import {
+  withRouter,
+  Switch,
+  Route,
+  Redirect,
+  Link
+} from 'react-router-dom'
+import {
+  // CssBaseline,
+  ThemeProvider,
+  createTheme,
+  Box
+} from '@mui/material'
+import {
+  darkTheme,
+  lightTheme
+} from './themes/basic'
 import HomeHeader from './components/Header';
-
-// const theme = createTheme({
-//   mode: 'dark',
-//   palette: {
-//     primary: {
-//       main: '#fff'
-//     },
-//     secondary: {
-//       main: '#351436'
-//     }
-//   }
-// })
+import About from './components/About'
+import Resume from './components/Resume'
+import Blog from './components/Blog'
+import Contact from './components/Contact'
 
 class App extends React.Component {
+  #title = 'Aaron Peter Samuel'
+  #defaultTheme = createTheme(darkTheme)
+  #drawerWidth = 200
 
   constructor(props) {
     super(props)
+    this.props = { ...props }
     this.state = {
-      colorMode: 'dark'
+      colorMode: 'dark',
+      theme: this.#defaultTheme
     }
     this.toggleColorMode = this.toggleColorMode.bind(this)
-    // this.eventHandler = this.eventHandler.bind(this)
-    this.theme = createTheme({
-      mode: 'dark',
-      shape: {
-        borderRadius: 4
-      },
-      palette: {
-        primary: {
-          main: '#fff',
-        },
-        secondary: {
-          main: '#351436',
-        },
-      },
-    });
     this.darkTheme = createTheme(darkTheme)
     this.lightTheme = createTheme(lightTheme)
   }
@@ -47,27 +45,69 @@ class App extends React.Component {
   toggleColorMode(componentState) {
     this.setState(state => ({
       ...state,
+      theme: componentState.darkMode
+      ? this.darkTheme
+      : this.lightTheme,
       colorMode: componentState.darkMode
       ? 'dark'
       : 'light'
     }))
   }
 
-  eventHandler(state) {
-    console.log(state)
+  componentDidMount() {
+    console.log(this.props)
   }
 
+  // eventHandler(state) {
+  //   console.log(state)
+  // }
+
   render() {
+    const title = this.#title
     const { colorMode } = this.state
+    const selectedTheme = colorMode === 'dark'
+      ? this.darkTheme
+      : this.lightTheme
     return (
       <div className="App">
-        <ThemeProvider theme={ colorMode === 'dark' ? this.darkTheme : this.lightTheme }
-        >
-          <HomeHeader onChange={this.toggleColorMode}/>
+        <ThemeProvider theme={selectedTheme}>
+          {/* Header Navigation, sticky across all views */}
+          <HomeHeader
+            {...this.props}
+            title={title}
+            drawerWidth={this.#drawerWidth}
+            theme={selectedTheme}
+            onChange={this.toggleColorMode}
+          />
+
+          {/* Content Section */}
+          <Box
+            style={{
+              paddingTop: 10,
+            }}
+          >
+            <Switch>
+              <Route exact path="/">
+                <p>Testing 123</p>
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/resume">
+                <Resume />
+              </Route>
+              <Route path="/blog">
+                <Blog {...this.props}/>
+              </Route>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+            </Switch>
+          </Box>
         </ThemeProvider>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
