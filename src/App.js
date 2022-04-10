a8a5663bd1eddd2ce349c2bf8e6a8651adb34d3c
apsamuel <aaron.psamuel@gmail.com>
2022-04-09 22:33:36 -0400

    refactor: change to class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { ThemeProvider, createTheme } from '@mui/material'
+import { darkTheme, lightTheme } from './themes/basic'
+import HomeHeader from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+// const theme = createTheme({
+//   mode: 'dark',
+//   palette: {
+//     primary: {
+//       main: '#fff'
+//     },
+//     secondary: {
+//       main: '#351436'
+//     }
+//   }
+// })
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      colorMode: 'dark'
+    }
+    this.toggleColorMode = this.toggleColorMode.bind(this)
+    // this.eventHandler = this.eventHandler.bind(this)
+    this.theme = createTheme({
+      mode: 'dark',
+      shape: {
+        borderRadius: 4
+      },
+      palette: {
+        primary: {
+          main: '#fff',
+        },
+        secondary: {
+          main: '#351436',
+        },
+      },
+    });
+    this.darkTheme = createTheme(darkTheme)
+    this.lightTheme = createTheme(lightTheme)
+  }
+
+  toggleColorMode(componentState) {
+    this.setState(state => ({
+      ...state,
+      colorMode: componentState.darkMode
+      ? 'dark'
+      : 'light'
+    }))
+  }
+
+  eventHandler(state) {
+    console.log(state)
+  }
+
+  render() {
+    const { colorMode } = this.state
+    return (
+      <div className="App">
+        <ThemeProvider theme={ colorMode === 'dark' ? this.darkTheme : this.lightTheme }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <HomeHeader onChange={this.toggleColorMode}/>
+        </ThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
